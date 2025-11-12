4f7099bbd2655a46a720529c4f38fd9f31466ce4
Bilal Quamine <99098734+0authnt@users.noreply.github.com>
2025-11-12 16:55:36 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AdisSite from "./adis_site.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AdisSite />
-  </React.StrictMode>
-);
